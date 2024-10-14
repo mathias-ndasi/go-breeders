ed93b1eb7f8d6690d7680331b3c066e327685f36
Mathias Ndasi <mathiasndasi@gmail.com>
2024-10-14 14:52:01 +0100

    web setup

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func (app *application) ShowHome(responseWriter http.ResponseWriter, request *http.Request){
 	app.renderTemplate(responseWriter, "home.page.gohtml", nil)
+}
+
+func (app *application) ShowPage(responseWriter http.ResponseWriter, request *http.Request){
+	page := chi.URLParam(request, "page")
+	app.renderTemplate(responseWriter, fmt.Sprintf("%s.page.gohtml", page), nil)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,7 +14,11 @@ func (app *application) routes() http.Handler {
 	multiplexer.Use(middleware.Recoverer);
 	multiplexer.Use(middleware.Timeout(60 * time.Second));
 
+	fileServer := http.FileServer(http.Dir("./static"))
+	multiplexer.Handle("/static/*", http.StripPrefix("/static/", fileServer))
+	
 	multiplexer.Get("/", app.ShowHome)
+	multiplexer.Get("/{page}", app.ShowPage)
 
 	return multiplexer;
 }
